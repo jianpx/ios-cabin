536a235d6d42a1ed80d6787be69212b1b1ff61eb
jianpx <jianpx86@gmail.com>
2013-07-14 23:41:02 +0800

    update example of setting pagecontrol pos.

--- PagedImageScrollView/Test/Test/ViewController.m
+++ PagedImageScrollView/Test/Test/ViewController.m
@@ -21,14 +21,9 @@
     //sample invoke code. 3 lines!
     PagedImageScrollView *pageScrollView = [[PagedImageScrollView alloc] initWithFrame:CGRectMake(0, 0, 320, 120)];
     [pageScrollView setScrollViewContents:@[[UIImage imageNamed:@"xyq.jpeg"], [UIImage imageNamed:@"x2.jpeg"], [UIImage imageNamed:@"xyq.jpeg"], [UIImage imageNamed:@"x2.jpeg"]]];
+    //easily setting pagecontrol pos, see PageControlPosition defination in PagedImageScrollView.h
+    pageScrollView.pageControlPos = PageControlPositionCenterBottom;
     [self.view addSubview:pageScrollView];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
